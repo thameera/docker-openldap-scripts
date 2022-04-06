188104ac95510d283f1bd7ccb800d278d339e3c7
Thameera Senanayaka <me@thameera.com>
2022-04-06 19:31:32 +1000

    Update container versions

--- config.example.sh
+++ config.example.sh
@@ -3,17 +3,17 @@
 #Environment Variables
 OPENDAP_CONTAINER_NAME="ldap-service"
 PHP_CONTAINER_NAME="phpldapadmin-service"
-LDAP_ORGANISATION="DSE Inc."
-LDAP_DOMAIN="auth0dse.local"
-LDAP_HOSTNAME="auth0dse.local"
-LDAP_BASE_DN="dc=auth0dse,dc=local"
-LDAP_USER="cn=admin,dc=auth0dse,dc=local"
+LDAP_ORGANISATION="Acme Inc."
+LDAP_DOMAIN="acme.com"
+LDAP_HOSTNAME="Acme.com"
+LDAP_BASE_DN="dc=acme,dc=com"
+LDAP_USER="cn=admin,dc=acme,dc=com"
 PHPLDAPADMIN_CONTAINER_NAME="phpldapadmin-service"
 PHPLDAPADMIN_HOSTNAMENAME="phpldapadmin.auth0dse.local"
 # Add users config
 LDAP_USER_DIR="./ldap-users"
 CONTAINER_LDIF_PATH="/container/service/slapd/assets/test"
 # LDAP DOCKER IMAGE
-LDAP_DOCKER_IMAGE="osixia/openldap:1.2.2"
+LDAP_DOCKER_IMAGE="osixia/openldap:1.5.0"
 # PHP LDAP DOCKER IMAGE
-PHP_LDAP_DOCKER_IMAGE="osixia/phpldapadmin:0.7.2"
+PHP_LDAP_DOCKER_IMAGE="osixia/phpldapadmin:0.9.0"
